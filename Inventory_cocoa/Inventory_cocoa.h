aa2ff22f9464fb2eec793de47958946704b35062
Aruna-ios <arunakumari.d@cartrade.com>
2025-03-17 17:34:53 +0530

    Initial Commit

---
+++ Inventory_cocoa/Inventory_cocoa.h
@@ -0,0 +1,18 @@
+//
+//  Inventory_cocoa.h
+//  Inventory_cocoa
+//
+//  Created by Chandini on 17/03/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Inventory_cocoa.
+FOUNDATION_EXPORT double Inventory_cocoaVersionNumber;
+
+//! Project version string for Inventory_cocoa.
+FOUNDATION_EXPORT const unsigned char Inventory_cocoaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Inventory_cocoa/PublicHeader.h>
+
+
---
+++ Inventory_cocoaTests/Inventory_cocoaTests.swift
@@ -0,0 +1,17 @@
+//
+//  Inventory_cocoaTests.swift
+//  Inventory_cocoaTests
+//
+//  Created by Chandini on 17/03/25.
+//
+
+import Testing
+@testable import Inventory_cocoa
+
+struct Inventory_cocoaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
